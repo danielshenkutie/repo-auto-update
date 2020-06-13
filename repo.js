bb4676368ce895b87255adbb26430118f394a30f
danielshenkutie <dr.dumz@gmail.com>
2020-06-13 16:28:24 +0200

    reop auto

--- repo.js
+++ repo.js
@@ -1,2 +1,21 @@
 const NodeGit = require('nodegit');
-var pathToRepo = require('path').resolve('../my-git-projects/my-project');
+var pathToRepo = require('path').resolve(
+  '/Users/danielshenkutie/Desktop/untitled folder/repo-auto-push'
+);
+var getMostRecentCommit = function (repository) {
+  return repository.getBranchCommit('master');
+};
+
+var getCommitMessage = function (commit) {
+  return commit.message();
+};
+
+NodeGit.Repository.open(pathToRepo)
+  .then(function (repo) {
+    console.log(
+      repo.getCommit().then((commit) => console.log(commit.message()))
+    );
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
